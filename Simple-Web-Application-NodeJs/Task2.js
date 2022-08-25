95ec87a88f45180520d07c48d375a022f1956758
ArmanDipu <armanhossain5@iut-dhaka.edu>
2022-08-25 21:25:01 +0600

    Added functionality for "If File Not Found"

--- Simple-Web-Application-NodeJs/Task2.js
+++ Simple-Web-Application-NodeJs/Task2.js
@@ -2,7 +2,7 @@ var http = require('http')
 var fs = require('fs')
 
 const server = http.createServer((req, res)=>{
-  const path = "./public" + req.url;
+  const path = "./publik" + req.url;
 
   if(fs.existsSync(path)) {
     const ext = path.substring(path.lastIndexOf('.'));
@@ -17,6 +17,10 @@ const server = http.createServer((req, res)=>{
       return res.end();
     });
   } else {
-    console.log(req.url)
+    console.log("Requested For: "+req.url)
+    console.log("Available Files/Folder:")
+    fs.readdirSync("./public").forEach(file => {
+      console.log(file);
+    })
   }
 }).listen(8000)
