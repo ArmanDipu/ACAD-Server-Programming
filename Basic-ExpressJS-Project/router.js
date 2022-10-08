ef78b21359696cb105b9afd729c5bbca2d51244b
Arman Hossain Dipu Khan <80636334+ArmanDipu@users.noreply.github.com>
2022-10-08 16:05:37 +0600

    made education.ejs  dynamic with JSON data

--- Basic-ExpressJS-Project/router.js
+++ Basic-ExpressJS-Project/router.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const fs = require("fs")
 
 router.get("/",(req,res)=>{
     res.send("Home Page Here")
@@ -10,12 +11,21 @@ router.get("/about",(req,res)=>{
 })
 
 router.get("/cv",(req,res)=>{
+    educations = fs.readFileSync("data/education",{ encoding: "utf-8" })
+    educations = JSON.parse(String(educations)) 
+
+    edus = []
+    for(let key in educations){
+        edus.push(educations[key])
+    }
+
     res.render("cv",{
         name: "Arman Hossain Dipu",
         profession : "Software Engineer",
         address : "Dhaka, Bangladesh",
         email : "armanshuvo2000@gmail.com",
-        phone: "01611595616"
+        phone: "01611595616",
+        educations:edus
     })
 })
 
