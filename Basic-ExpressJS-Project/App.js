b531f703044ce7a813689d6f472bd230b372b450
Arman Hossain Dipu Khan <80636334+ArmanDipu@users.noreply.github.com>
2022-10-08 11:49:05 +0600

    Created Router

--- Basic-ExpressJS-Project/App.js
+++ Basic-ExpressJS-Project/App.js
@@ -1,21 +1,12 @@
-// const http = require("http")
 const express = require("express")
+const router = require("./router")
 const app = express()
 
 const hostname = "127.0.0.1"
 const port = 3000
 
-// const server = http.createServer((req,res)=>{
-//     res.writeHead(200,{"Content-type": "text/plain"})
-//     res.end("Intitial Server")
-// })
-app.get("/",(req,res)=>{
-    res.send("Initial Express Server")
-})
+app.use(router)
 
-// server.listen(port,hostname,()=>{
-//     console.log(`Server running at http://${hostname}:${port}/`)
-// })
 app.listen(port,()=>{
     console.log(`Server running at http://${hostname}:${port}/`)
 })
