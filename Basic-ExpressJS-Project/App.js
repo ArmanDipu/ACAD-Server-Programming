c01cdce998b86841d5ea7320cd28ef5f63c693a3
Arman Hossain Dipu Khan <80636334+ArmanDipu@users.noreply.github.com>
2022-10-08 12:11:56 +0600

    ejs template engine setup

--- Basic-ExpressJS-Project/App.js
+++ Basic-ExpressJS-Project/App.js
@@ -6,6 +6,8 @@ const hostname = "127.0.0.1"
 const port = 3000
 
 app.use(express.static("./public"))
+app.set("view engine","ejs")
+app.set("views", __dirname+"/views")
 app.use(router)
 
 app.listen(port,()=>{
