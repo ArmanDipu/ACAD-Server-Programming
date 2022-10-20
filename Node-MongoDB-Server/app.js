e82d517aabfc53ca7f9a40a229e134bf4881c2a0
Arman Hossain Dipu Khan <80636334+ArmanDipu@users.noreply.github.com>
2022-10-20 22:55:40 +0600

    Created Basic Router

--- Node-MongoDB-Server/app.js
+++ Node-MongoDB-Server/app.js
@@ -1,11 +1,9 @@
 const express = require("express")
+const router = require("./routes")
 
 const port = 3000
 const app = express()
-
-app.get('/', (req, res) => {
-    res.send('Test Server')
-  })
+app.use(router)
 
 app.listen(port, ()=>{
     console.log(`App is running at http://localhost:${port}`)
