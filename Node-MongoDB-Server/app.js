d8b260cc14a4ce737e27e576679b4cc23a664aef
Arman Hossain Dipu Khan <80636334+ArmanDipu@users.noreply.github.com>
2022-10-20 23:08:31 +0600

    Added Homepage (ejs)

--- Node-MongoDB-Server/app.js
+++ Node-MongoDB-Server/app.js
@@ -5,6 +5,9 @@ const port = 3000
 const app = express()
 app.use(router)
 
+app.set("view engine", "ejs")
+app.set("views",__dirname+"/views")
+
 app.listen(port, ()=>{
     console.log(`App is running at http://localhost:${port}`)
 })
