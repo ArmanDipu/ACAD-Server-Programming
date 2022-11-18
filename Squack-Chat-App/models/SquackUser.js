245e3ab857b4aa7fd002e2886353774dd4d64140
Arman Hossain Dipu Khan <80636334+ArmanDipu@users.noreply.github.com>
2022-11-18 17:40:00 +0600

    Added Models

---
+++ Squack-Chat-App/models/SquackUser.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose")
+
+const SquackUserSchema = mongoose.Schema({
+    name:{
+        type: String
+    }
+})
---
+++ Squack-Chat-App/models/messeges.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const messegeSchema = mongoose.Schema({
+    from :{
+        required : true,
+        type : String
+    },
+    to :{
+        required : true,
+        type : String
+    },
+    msg:{
+        required : true,
+        type : String
+    }
+})
