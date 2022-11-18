8832ecf127341bec723e3ef78a0dce9d940275fe
Arman Hossain Dipu Khan <80636334+ArmanDipu@users.noreply.github.com>
2022-11-18 19:31:43 +0600

    Dynamic Users

--- Squack-Chat-App/controllers/indexController.js
+++ Squack-Chat-App/controllers/indexController.js
@@ -4,6 +4,8 @@ const messegeModel = require("../models/messeges.js")
 const getIndexPage = async (req,res)=>{
     let data = []
     let msgs = []
+    let users = []
+    var currentUser
 
     try{
         data = await messegeModel.find()
@@ -11,14 +13,23 @@ const getIndexPage = async (req,res)=>{
         data.forEach((messegeSchema)=>{
             msgs.push({from: messegeSchema.from, msg: messegeSchema.msg})
         })
+
+        data = await squackUserModel.find()
+        data.forEach((SquackUserSchema)=>{
+            users.push({name: SquackUserSchema.name})
+        })
+
+        currentUser = msgs[msgs.length-1].from
+
     }catch(err){
         console.log(err)
     }finally{
-        res.render("index",{ msgs: msgs})
+        res.render("index",{msgs:msgs , users:users, currentUser})
     }
 }
 
 const postMessege = (req,res) =>{
+
     const data = new messegeModel({
         from: req.body.from,
         to: req.body.to,
@@ -29,6 +40,7 @@ const postMessege = (req,res) =>{
     }).catch((err)=>console.log(err)).finally(()=>{
         res.redirect("/")
     })
+    this.currentUser =  req.body.from
 }
 
 module.exports = {getIndexPage, postMessege}
